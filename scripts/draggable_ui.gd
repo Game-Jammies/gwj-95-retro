cce304533a86f4a77e4716aab415b5bfbbef0f68
Thewtus <thewtus@gmail.com>
2026-07-10 20:58:41 -0500

    created a generic window prototype

---
+++ scripts/draggable_ui.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+var draggingDistance
+var dir
+var dragging
+var newPosition = Vector2()
+
+@export var draggingSpeed = Vector2(30, 30)
+
+var mouse_in = false
+
+func _ready():
+	pass
+	
+	
+func _input(event):
+	"""Handles mouse inputs"""
+	if event is InputEventMouseButton:
+		#if mouse click and held, allow window to be dragged
+		if event.is_pressed() && mouse_in:
+			draggingDistance = position.distance_to(get_viewport().get_mouse_position())
+			dir = (get_viewport().get_mouse_position() - position).normalized()
+			dragging = true
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+		else:
+			dragging = false
+			
+	elif event is InputEventMouseMotion:
+		#drag the window when mouse is moved
+		if dragging:
+			newPosition = get_viewport().get_mouse_position() - draggingDistance * dir
+
+func _physics_process(delta):
+	#slides the window around
+	if dragging:
+		velocity = (newPosition - position) * draggingSpeed #vector2 is dragging speed
+		move_and_slide() #cool function that does stuff for me 
+	
+func mouse_entered():
+	mouse_in = true
+
+func mouse_exited():
+	mouse_in = false
+
+func _on_close_pressed():
+	#destroy itself when closed
+	get_parent().remove_child(self)
+	self.queue_free()
