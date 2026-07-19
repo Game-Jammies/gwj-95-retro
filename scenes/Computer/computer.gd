42df6dabf31cd6ecdad57fc0408c365e34453dfa
Thewtus <thewtus@gmail.com>
2026-07-19 14:13:21 -0500

    add tutorial window

--- scenes/Computer/computer.gd
+++ scenes/Computer/computer.gd
@@ -1,1 +1,10 @@
 class_name Computer extends Control
+
+@export var window_load: PackedScene
+@export var spawn_pos: Vector2
+
+func _ready() -> void:
+	#spawn the tutorial window
+	var window = window_load.instantiate()
+	window.global_position = spawn_pos
+	get_tree().root.add_child.call_deferred(window)
