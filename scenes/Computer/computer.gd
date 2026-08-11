997022ef84b1ecba1db2bc9d9b8596819585c3e5
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2026-08-10 19:33:37 -0500

    adjusted min sizes for request panels

--- scenes/Computer/computer.gd
+++ scenes/Computer/computer.gd
@@ -1,10 +1,11 @@
 class_name Computer extends Control
 
-@export var window_load: PackedScene
-@export var spawn_pos: Vector2
+@export var tutorial_scene: PackedScene
 
 func _ready() -> void:
-	#spawn the tutorial window
-	var window = window_load.instantiate()
-	window.global_position = spawn_pos
-	get_tree().root.add_child.call_deferred(window)
+	var tutorial: Control = tutorial_scene.instantiate()
+	
+	# center the tutorial window
+	tutorial.global_position = (get_viewport_rect().size - tutorial.size) / 2 
+	
+	get_tree().root.add_child.call_deferred(tutorial)
